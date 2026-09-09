437d821d0b40a231524aa1f2d49bc4fd649c1461
dkooll <dennis.kool@cloudnation.nl>
2026-09-09 09:23:43 +0200

    feat: azurerm provider 5 upgrade BREAKING CHANGE: this change causes recreates

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "firewall_policy" {
   description = "contains frontdoor firewall configuration"
-  value       = azurerm_cdn_frontdoor_firewall_policy.policy
+  value       = azurerm_cdn_frontdoor_firewall_policy.this
 }
 
 output "security_policy" {
   description = "contains frontdoor security policy configuration"
-  value       = azurerm_cdn_frontdoor_security_policy.policy
+  value       = azurerm_cdn_frontdoor_security_policy.this
 }
